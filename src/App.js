86017f31e762e3da5158ea7deccd14a83c277ae5
Oleg Knyazev <knyazev751@gmail.com>
2018-03-15 11:06:20 +0300

    Add bootstrap and reactstrap wrapper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -15,7 +13,7 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <h1>Transactions</h1>
         <table class="table">
           <thead>
